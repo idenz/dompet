6384e1a86c29bb6aacc5b1c303626af82e76ebfb
iden <dennydharmawan717@gmail.com>
2022-06-26 14:36:34 +0700

    USER -> update user model

--- applications/user/user.model.go
+++ applications/user/user.model.go
@@ -1,7 +1,8 @@
 package users
 
 type UserModel struct {
-	username string `json:"username" bson:"user_name"`
-	email    string `json:"email" bson:"user_email"`
-	password string `json:"password" bson:"user_password"`
+	Username string `json:"username"`
+	Email    string `json:"email"`
+	Password string `json:"password"`
+	IsActive bool   `json:"isActive"`
 }
