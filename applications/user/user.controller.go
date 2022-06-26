6758656298165152732d6f61d66c5841e4ab409a
iden <dennydharmawan717@gmail.com>
2022-06-26 14:37:14 +0700

    USER -> add create controller user

--- applications/user/user.controller.go
+++ applications/user/user.controller.go
@@ -1,6 +1,7 @@
 package users
 
 import (
+	"encoding/json"
 	"net/http"
 
 	"github.com/labstack/echo/v4"
@@ -18,7 +19,33 @@ func New(_userService IUserService) *UserController {
 }
 
 func (u *UserController) CreateUser(c echo.Context) (err error) {
-	return c.JSON(http.StatusOK, "denny")
+
+	payload := new(UserRegisterRequest)
+	if err = c.Bind(payload); err != nil {
+		return err
+	}
+
+	if err = c.Validate(payload); err != nil {
+		return err
+	}
+
+	user, err := u.UserService.CreateUser(payload)
+	if err != nil {
+		return nil
+	}
+
+	result := new(UserResponse)
+	bytes, err := json.Marshal(&user)
+	if err != nil {
+		return err
+	}
+
+	err = json.Unmarshal(bytes, &result)
+	if err != nil {
+		return nil
+	}
+
+	return c.JSON(http.StatusOK, result)
 }
 
 func (u *UserController) GetUser(c echo.Context) (err error) {
