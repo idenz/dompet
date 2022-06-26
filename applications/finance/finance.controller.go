9bd5c2067a13738b02b3edf16af7b1a8c80c2a30
iden <dennydharmawan717@gmail.com>
2022-06-26 15:36:58 +0700

    FINANCE -> add controller create

--- applications/finance/finance.controller.go
+++ applications/finance/finance.controller.go
@@ -1,6 +1,11 @@
 package finances
 
-import "github.com/labstack/echo/v4"
+import (
+	"encoding/json"
+	"net/http"
+
+	"github.com/labstack/echo/v4"
+)
 
 type Controller struct {
 	Service IService
@@ -14,9 +19,35 @@ func New(_service IService) *Controller {
 }
 
 func (c *Controller) Create(ctx echo.Context) (err error) {
-	return nil
+
+	payload := new(CreateRequest)
+	if err = ctx.Bind(payload); err != nil {
+		return err
+	}
+
+	if err = ctx.Validate(payload); err != nil {
+		return err
+	}
+
+	finance, err := c.Service.Create(payload)
+	if err != nil {
+		return err
+	}
+
+	result := new(Response)
+	bytes, err := json.Marshal(&finance)
+	if err != nil {
+		return err
+	}
+
+	err = json.Unmarshal(bytes, &result)
+	if err != nil {
+		return err
+	}
+
+	return ctx.JSON(http.StatusOK, result)
 }
 
 func (c *Controller) GetAll(ctx echo.Context) (err error) {
-	return nil
+	return ctx.JSON(http.StatusOK, "ok")
 }
--- applications/finance/finance.route.go
+++ applications/finance/finance.route.go
@@ -5,7 +5,7 @@ import "github.com/labstack/echo/v4"
 func (finance *Controller) Route(g *echo.Group) {
 
 	path := g.Group("")
-	path.POST("/", finance.Create)
-	path.GET("/", finance.GetAll)
+	path.POST("", finance.Create)
+	path.GET("", finance.GetAll)
 
 }
